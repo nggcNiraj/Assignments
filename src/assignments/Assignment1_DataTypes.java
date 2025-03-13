d29e55405dae3148704e28d29699ad0f30879690
Niraj Chavan <chavaniraj22@outlook.com>
2025-03-13 09:38:26 +0530

    Assignments

--- src/assignments/Assignment1_DataTypes.java
+++ src/assignments/Assignment1_DataTypes.java
@@ -2,9 +2,13 @@ package assignments;
 
 public class Assignment1_DataTypes {
 
+//	private static final String Temperature = null;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		float temparature = 25.5f;
+			System.out.println("Temperature of a city in Degree Celcius is :"+temparature);
 	}
 
 }
+   
